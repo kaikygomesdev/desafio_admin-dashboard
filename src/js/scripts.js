6a65ee0a596ba5f4975d67bb3d85352adb99b698
kaikygomesdev <kaikygomes340@gmail.com>
2025-11-01 15:24:12 -0300

    feat:js-function_activeLink

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
 // add hovered class in selected list item
 let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
